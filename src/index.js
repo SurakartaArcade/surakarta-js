d0a77fbe9bcee188e47c0048f9676740a64ffbd1
Shukant Pal <shukantpal@outlook.com>
2020-03-31 16:37:09 -0400

    Build changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
  * @namespace SK
  */
 
-export { RED_PLAYER, BLACK_PLAYER, NOT_FILLED, Surakarta } from './Surakarta'
-export { Move } from './Move'
-export { Position } from './Position'
-export * from './traverse-utils'
-export * from './step-utils'
-export * from './PlayerStats'
+export { RED_PLAYER, BLACK_PLAYER, NOT_FILLED, Surakarta } from "./Surakarta";
+export { Move } from "./Move";
+export { Position } from "./Position";
+export * from "./traverse-utils";
+export * from "./step-utils";
+export * from "./PlayerStats";
